a484443bddfb3eee2431bfb9300d3e7766d645e9
ducthangchin <trinhducthang201@gmail.com>
2022-11-13 02:22:56 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {Swiper, SwiperSlide} from 'swiper/react'
 
 
-import './App.css';
+import './app.scss';
+
+import Navbar from './component/navbar/Navbar';
+import Footer from './component/footer/Footer';
 import About from './pages/about/About';
 import Home from './pages/home/Home';
 import Projects from './pages/projects/Projects';
+import Resume from './pages/resume/Resume';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/projects' element={<Projects/>}/>
-        </Routes>
+        <Navbar></Navbar>
+        
+        <div className='page-content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/resume' element={<Resume />} />
+          </Routes>
+        </div>
+{/* 
+        <Footer></Footer> */}
       </BrowserRouter>
+
       
     </div>
   );
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import './navbar.scss'
+
+
+import logo from '../../assets/logo.png'
+import { faHome, faFile , faAddressBook, faProjectDiagram } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+
+const navBarElement = [
+    {
+        name: 'Home',
+        icon: faHome,
+        path: '/'
+    },
+    {
+        name: 'About',
+        icon: faAddressBook,
+        path: '/about'
+    },
+    {
+        name: 'Projects',
+        icon: faProjectDiagram,
+        path: '/projects'
+    },
+    {
+        name: 'Resume',
+        icon: faFile,  
+        path: '/resume'
+    }
+]
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+    const navigate = useNavigate()  
+
+    return (
+        <div className='nav-bar'>
+            <div className='logo' onClick={() => navigate('/')}>
+                <img src={logo}></img>
+                <div className='title'>Chin blog</div>
+            </div>
+
+            <div className='items'>
+                {navBarElement.map((item, key) => 
+                    <div className='element' key={key} onClick={() => navigate(item.path)}>
+                        <FontAwesomeIcon icon={item.icon} />    
+                        
+                        <div>{item.name}</div>    
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
